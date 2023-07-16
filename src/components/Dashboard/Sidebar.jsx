fafde3a41983cca1770bb664afe26d7eb49a3611
Shuyibe48 <gthrough48@gmail.com>
2023-07-16 19:36:12 +0600

    add product static

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { GrLogout } from 'react-icons/gr'
-import { FcSettings } from 'react-icons/fc'
+import { FcHome } from 'react-icons/fc'
 import { AiOutlineBars } from 'react-icons/ai'
 
 import { AuthContext } from '../../provider/AuthProvider'
@@ -54,7 +54,7 @@ const Sidebar = () => {
                                     referrerPolicy='no-referrer'
                                 />
                             </Link>
-                            <Link to='/'>Computer Shop</Link>
+                            <Link className='text-cyan-100' to='/'>Computer Shop</Link>
                         </div>
                         <div className='flex flex-col items-center mt-6 -mx-2'>
                             <Link to='/dashboard'>
@@ -75,12 +75,14 @@ const Sidebar = () => {
                         <nav>
                             <>
                                 <Link
+                                    onClick={handleToggle}
                                     to='dashboard/addAProduct'
                                     className='flex items-center px-4 py-2 mt-5 border-b border-gray-100 transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700'
                                 >
                                     <span className='mx-4 font-medium text-cyan-100'>Add A Product</span>
                                 </Link>
                                 <Link
+                                    onClick={handleToggle}
                                     to='dashboard/myProducts'
                                     className='flex items-center px-4 py-2 mt-5 border-b border-gray-100 transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700'
                                 >
@@ -100,11 +102,11 @@ const Sidebar = () => {
                             }`
                         }
                     >
-                        <FcSettings className='w-5 h-5' />
+                        <FcHome className='w-5 h-5' />
 
                         <span className='mx-4 text-gray-100 font-medium'>Home</span>
                     </NavLink>
-                    
+
                     <button
                         onClick={handleLogOut}
                         className='flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform'
--- src/pages/Dashboard/AddProduct.jsx
+++ src/pages/Dashboard/AddProduct.jsx
@@ -1,7 +1,163 @@
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+
 const AddProduct = () => {
+    const { user } = useContext(AuthContext)
+
+    console.log(user);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target
+        const className = event.target.className.value
+        const instructorName = user?.displayName
+        const email = user?.email
+        const availableSeats = event.target.availableSeats.value
+        const price = event.target.price.value
+
+        // Image Upload
+        const image = event.target.classImage.files[0]
+        const formData = new FormData()
+        formData.append('image', image)
+
+        const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY
+            }`
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(imageData => {
+                const imageUrl = imageData.data.display_url
+                const classData = {
+                    className,
+                    instructorName,
+                    email,
+                    availableSeats,
+                    price: parseFloat(price),
+                    image: imageUrl,
+                    host: {
+                        name: user?.displayName,
+                        image: user?.photoURL,
+                        email: user?.email,
+                    },
+                    status: 'pending',
+                    feedback: ''
+                }
+
+
+
+
+                // post class data to server
+                // addClass(classData)
+                //     .then(data => {
+                //         console.log(data)
+                //     })
+                //     .catch(err => console.log(err))
+
+
+
+                
+                form.reset()
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+
+    };
+
     return (
-        <div>
-            add product
+        <div className="flex justify-center items-center h-screen">
+            <form
+                className="bg-white shadow-lg rounded-lg p-8 w-full sm:w-3/4 md:w-1/2 lg:w-1/3"
+                onSubmit={handleSubmit}
+            >
+                <h2 className="text-2xl font-bold mb-8 text-center">Add A Product</h2>
+
+                <div className="mb-4">
+                    <label htmlFor="className" className="block mb-2 font-medium">
+                        Class Name
+                    </label>
+                    <input
+                        type="text"
+                        id="className"
+                        className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                        required
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="classImage" className="block mb-2 font-medium">
+                        Class Image
+                    </label>
+                    <input
+                        type="file"
+                        id="classImage"
+                        accept='image/*'
+                        required
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="instructorName" className="block mb-2 font-medium">
+                        Seller Name
+                    </label>
+                    <input
+                        type="text"
+                        id="instructorName"
+                        className="border border-gray-300 rounded-md w-full py-2 px-3 bg-gray-100"
+                        value={user?.displayName}
+                        readOnly
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="instructorEmail" className="block mb-2 font-medium">
+                        Seller Email
+                    </label>
+                    <input
+                        type="email"
+                        id="instructorEmail"
+                        className="border border-gray-300 rounded-md w-full py-2 px-3 bg-gray-100"
+                        value={user?.email}
+                        readOnly
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="availableSeats" className="block mb-2 font-medium">
+                        Available Product
+                    </label>
+                    <input
+                        type="number"
+                        id="availableSeats"
+                        className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                        required
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="price" className="block mb-2 font-medium">
+                        Price
+                    </label>
+                    <input
+                        type="number"
+                        id="price"
+                        className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                        required
+                    />
+                </div>
+
+                <div className="text-center">
+                    <button
+                        type="submit"
+                        className="bg-gray-800 hover:bg-gray-400 text-white font-medium py-2 px-4 rounded-md transition-colors duration-300"
+                    >
+                        Post
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
