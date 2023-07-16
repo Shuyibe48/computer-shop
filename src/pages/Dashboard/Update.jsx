181c9ad8c8f76ebb53c408dce729ccf1c884e04f
Shuyibe48 <gthrough48@gmail.com>
2023-07-17 01:45:38 +0600

    updated

--- src/pages/Dashboard/Update.jsx
+++ src/pages/Dashboard/Update.jsx
@@ -42,7 +42,7 @@ const Update = () => {
                         id="name"
                         className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         placeholder="Enter name"
-                        value={name}
+                        value={productName}
                         onChange={(e) => setProductName(e.target.value)}
                         required
                     />
